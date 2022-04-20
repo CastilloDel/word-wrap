25cfbd86c2fd66f430da9ca86fea8e76b5cf22c9
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-04-20 11:35:09 +0100

    Prefer breaking at spaces

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
 pub fn wrap_lines(lines: &str, limit: usize) -> String {
-    if lines.len() > limit {
-        format!(
-            "{}\n{}",
-            &lines[0..limit],
-            &wrap_lines(&lines[limit..], limit)
-        )
-    } else {
-        lines.to_string()
+    if lines.len() <= limit {
+        return lines.to_string();
     }
+    let space_before_limit = lines[0..limit].rfind(" ");
+    let (break_index, next_start) = match space_before_limit {
+        Some(val) => (val, val + 1),
+        None => (limit, limit),
+    };
+    format!(
+        "{}\n{}",
+        &lines[0..break_index],
+        &wrap_lines(&lines[next_start..], limit),
+    )
 }
 
 #[cfg(test)]
@@ -25,5 +29,11 @@ mod tests {
     fn should_break_if_the_line_is_larger_than_the_limit() {
         assert_eq!("123\n45", wrap_lines("12345", 3));
         assert_eq!("123\n456\n7", wrap_lines("1234567", 3));
+    }
+
+    #[test]
+    fn should_prefer_breaking_in_existing_spaces() {
+        assert_eq!("123\n45", wrap_lines("123 45", 4));
+        assert_eq!("123\n4578\n909", wrap_lines("123 4578909", 4));
     }
 }
