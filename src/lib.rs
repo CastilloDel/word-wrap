01f2a2dcf47ccd284eb82022d963411ae7885ffa
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-04-22 09:21:39 +0100

    Fix wrong recursive call

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,15 @@ pub fn wrap_lines(lines: &str, limit: usize) -> String {
         .unwrap()
 }
 
-fn wrap_line(lines: &str, limit: usize) -> String {
-    if lines.len() <= limit {
-        return lines.to_string();
+fn wrap_line(line: &str, limit: usize) -> String {
+    if line.len() <= limit {
+        return line.to_string();
     }
-    let space_before_limit = lines[0..limit].rfind(" ");
-    let line = &lines[0..(space_before_limit.unwrap_or(limit))];
-    let rest = &lines[space_before_limit.map(|val| val + 1).unwrap_or(limit)..];
+    let space_before_limit = line[0..limit].rfind(" ");
+    let first_slice = &line[0..(space_before_limit.unwrap_or(limit))];
+    let rest = &line[space_before_limit.map(|val| val + 1).unwrap_or(limit)..];
 
-    format!("{}\n{}", line, &wrap_lines(rest.trim(), limit),)
+    format!("{}\n{}", first_slice, &wrap_lines(rest.trim(), limit),)
 }
 
 #[cfg(test)]
